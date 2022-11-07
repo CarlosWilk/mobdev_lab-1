f09c773f4532b3c465c21836c35ec906c79cff8b
Wilker Paulino <carloswilkersp@hotmail.com>
2022-11-07 14:48:44 +0000

    created 4 pages

--- mobdev_ca3/src/app/app-routing.module.ts
+++ mobdev_ca3/src/app/app-routing.module.ts
@@ -7,9 +7,29 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+
+  {
+    path: 'characters',
+    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
+  },
+  {
+    path: 'episodes',
+    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
+  },
+  {
+    path: 'quotes',
+    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
+  },
+  {
+    path: 'death-count',
+    loadChildren: () => import('./pages/death-count/death-count.module').then( m => m.DeathCountPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
 ];
 
---
+++ mobdev_ca3/src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'characters',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+          },
+          // {
+          //   path: ':id',
+          //   loadChildren: () => import('../film-details/film-details.module').then( m => m.FilmDetailsPageModule)
+          // }
+        ]
+      },
+  {
+    path: 'death-count',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('../death-count/death-count.module').then( m => m.DeathCountPageModule)
+      },
+      // {
+      //   path: ':id',
+      //   loadChildren: () => import('../specimen-details/specimen-details.module').then( m => m.SpecimenDetailsPageModule)
+      // }
+    ]
+},
+{
+  path: 'episodes',
+  children: [
+    {
+      path: '',
+      loadChildren: () => import('../episodes/episodes.module').then( m => m.EpisodesPageModule)
+    },
+    // {
+    //   path: ':id',
+    //   loadChildren: () => import('../spaceship-details/spaceship-details.module').then( m => m.SpaceshipDetailsPageModule)
+    // }
+  ]
+}
+    ]
+  },
+  {
+    path: 'quotes',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('../quotes/quotes.module').then( m => m.QuotesPageModule)
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/films',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
